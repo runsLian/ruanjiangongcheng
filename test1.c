771b0b39d2bec9f9137503849c02aea32874b808
runsLian <100836938+runsLian@users.noreply.github.com>
2022-05-18 15:25:28 +0800

    Initial commit

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+ int a,i;
+ printf("输入数组个数\n");
+ scanf("%d\n", &a);
+ char A[a];
+ for ( i = 0; i <=a; i++)
+ {
+     printf("输入")
+ }
+ 
+}
+//给定一个整数数组，计算该数组最大
+
+ 
