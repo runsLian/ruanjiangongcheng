f142454510f0b4ff8dc9e49f3e402b328501091a
runsLian <100836938+runsLian@users.noreply.github.com>
2022-05-18 16:24:58 +0800

    第一次软件工程作业

--- test1.c
+++ test1.c
@@ -1,16 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
- int a,i;
- printf("输入数组个数\n");
- scanf("%d\n", &a);
- char A[a];
- for ( i = 0; i <=a; i++)
- {
-     printf("输入")
- }
- 
+int main(){//给定一个数组，计算该数组最大子数组
+    int a;
+    printf("输入数组个数\n");
+    scanf("%d", &a);
+    int v[a];
+    printf("请输入数组中元素值\n");
+    for (int i = 0; i <a; i++)
+    {
+        printf("输入数组中第%d个数", i+1);
+        scanf("%d", &v[i]);
+    }
+    for ( int i = 0; i < a; i++)
+    {
+        printf("数组中第%d个数为%d\n", i + 1, v[i]);
+    }
+    int max = 0;
+    for (int i = 0; i < a; i++) 
+    {
+        int sum = v[i];
+        if (max<sum)
+        {
+            max = sum;
+        }
+        
+        for (int j = i + 1; j < a; j++)
+        {
+            sum += v[j];
+            if (sum > max)
+            {
+                max = sum;
+            }
+        }
+     }
+    printf("最大子数组之和为%d", max);
 }
-//给定一个整数数组，计算该数组最大
-
- 
